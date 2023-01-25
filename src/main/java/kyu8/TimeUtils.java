fe2500f86494019f5565393146f774a860fc57f4
Tomek <wartego@wp.pl>
2023-01-25 16:25:17 +0100

    TimeUtils repair

--- src/main/java/kyu8/TimeUtils.java
+++ src/main/java/kyu8/TimeUtils.java
@@ -1,23 +1,38 @@
 package kyu8;
 
 class TimeUtils {
-//    public static String convertTime(int timeDiff) {
-//
-//
-//
-//    }
-public static void main(String[] args) {
-     int timeDiff =90061;
-    int day = 0;
-    int houres = 0;
-    int min = 0;
-    int seconds = 0;
+    public static String convertTime(int timeDiff) {
+        int day = 0, hour = 0, minute = 0, second = 0;
+        int timeAux = timeDiff;
+        if(timeAux < 0){
+            timeAux *= -1;
+        }
 
+        while(timeAux > 0){
+            if(timeAux >= 60*60*24){
+                day ++;
+                timeAux -= 60*60*24;
+            } else if(timeAux >= 60*60){
+                hour ++;
+                timeAux -= 60*60;
+            } else if (timeAux >= 60) {
+                minute ++;
+                timeAux -= 60;
+            } else {
+                second = timeAux;
+                timeAux = 0;
+            }
 
-    seconds = timeDiff/(60*60*24) / min;
-    day = timeDiff/(60*60*24);
-    houres = timeDiff/(60*60*24) / day;
-    min = timeDiff/(60*60*24) / houres ;
+        }
 
-}
+        if(timeDiff < 0){
+            day *= -1;
+            hour *= -1;
+            minute *= -1;
+            second *= -1;
+        }
+
+        return day + " " + hour + " " + minute + " " + second ;
+
+    }
 }
