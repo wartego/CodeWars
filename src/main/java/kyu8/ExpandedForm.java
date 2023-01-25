280ece77746746f0c927ae1e7266ee1ea47ed309
Tomek <wartego@wp.pl>
2023-01-25 22:34:36 +0100

    Test + PrimeNumbers

---
+++ src/main/java/kyu8/ExpandedForm.java
@@ -0,0 +1,35 @@
+package kyu8;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ExpandedForm
+{
+    public static String expandedForm(int num)
+    {
+        String value = String.valueOf(num);
+        String text = " ";
+        int tempValue = 0;
+        int counter = 10;
+        List<String> tablica = new ArrayList<>();
+        for (int i = 0; i < value.length(); i++) {
+            tempValue = num%counter;
+            if(tempValue != 0){
+                tablica.add(0, String.valueOf(tempValue));
+            }
+
+            num -= tempValue;
+            counter *=10;
+        }
+        String textEnd = null;
+
+             textEnd = String.join(" + ", tablica);
+
+        return textEnd;
+    }
+
+    public static void main(String[] args) {
+        String s = ExpandedForm.expandedForm(12);
+        System.out.println(s);
+    }
+}
