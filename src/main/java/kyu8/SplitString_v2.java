323c11411bf65524e0748da32ca0de224a491d04
Tomek <wartego@wp.pl>
2023-01-31 20:28:06 +0100

    SplitString Warrior v2

---
+++ src/main/java/kyu8/SplitString_v2.java
@@ -0,0 +1,17 @@
+package kyu8;
+import java.lang.StringBuilder;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+public class SplitString_v2 {
+
+        static String toCamelCase(String s) {
+            if(s.isEmpty())
+                return s;
+            String ws[] = s.split("[-_]");
+            return ws[0] + Arrays.stream(ws)
+                    .skip(1)
+                    .map(wd -> Character.toUpperCase(wd.charAt(0)) + wd.substring(1))
+                    .collect(Collectors.joining(""));
+        }
+    }
+
